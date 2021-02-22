75f3daf65add525285cb81a783413cc47af40efd
Delphine-Jean <delphinejean92@yahoo.fr>
2021-02-22 21:33:03 +0100

    API Python project Flask changes

--- Portfolio_Data/API Python/Flask REST API/test.py
+++ Portfolio_Data/API Python/Flask REST API/test.py
@@ -2,9 +2,14 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1" , {"likes":10, "name":"delphine","views":27})
-print(response.json())
-input()
+data = [{"likes":10, "name":"delphine","views":27},
+        {"likes":110, "name":"tim","views":2715},
+        {"likes":4510, "name":"lisa","views":2017}]
 
-response = requests.get(BASE + "video/6")
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i),data[i])
+    print(response.json())
+
+input()
+response = requests.get(BASE + "video/2")
 print(response.json())
