b8802e7afdd8690591fe6c663b9a0413568a7bc1
Delphine-Jean <delphinejean92@yahoo.fr>
2021-03-04 22:53:35 +0100

    Airflow ETL MAJ

---
+++ Portfolio_Data/Data_engineering/Airflow/ETL - BigQuery/main.py
@@ -0,0 +1,6 @@
+https://gitlab.com/xavki/tutoriels-python/-/blob/master/27-google-api/slides.md
+
+
+KEY_API = AIzaSyDfRkzG8Sz5LKn6pLdpTOU0H6EXYFuP_iM
+
+https://github.com/tuanavu/airflow-tutorial/blob/v0.6/examples/gcloud-example/dags/bigquery_github/bigquery_github_trends.py 
--- Portfolio_Data/Data_engineering/Airflow/ETL - Cryptos/main.py
+++
@@ -1,4 +0,0 @@
-https://gitlab.com/xavki/tutoriels-python/-/blob/master/27-google-api/slides.md
-
-
-KEY_API = AIzaSyDfRkzG8Sz5LKn6pLdpTOU0H6EXYFuP_iM
