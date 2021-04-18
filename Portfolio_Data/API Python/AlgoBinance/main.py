8ebe16d32389e2dcb0a4f2782f3744ac18adca62
Delphine-Jean <delphinejean92@yahoo.fr>
2021-04-18 23:08:56 +0200

    init AlgoBinance project trading bot

---
+++ Portfolio_Data/API Python/AlgoBinance/main.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+import logging
+from connectors.binance_futures import BinanceClientFutures
+
+logger = logging.getLogger()
+
+logger.setLevel(logging.INFO)
+
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s %(levelname)s :: %(message)s")
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler("info.log")
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+logger.debug("message is important only when dubugging")
+logger.info("message for basic informations")
+logger.warning("message to pay attention")
+logger.error("message helps to debug an error")
+
+if __name__ == '__main__':
+
+    binance = BinanceClientFutures(True)
+    print(binance.get_historical_candles("BTCUSDT", "1h"))
+    root = tk.Tk()
+    root.mainloop()
+
+
+
+
+
+
