08fabc155381f6c0f875dabe4e0d3aebda6fe2f6
Delphine-Jean <delphinejean92@yahoo.fr>
2021-06-14 23:25:10 +0200

    Crypto Binance ETL  Day 13 -  correct errors variable env

---
+++ Portfolio_Data/Data_engineering/Binance_Project_Bot/venv/database.py
@@ -0,0 +1,20 @@
+from google.cloud import bigquery
+
+client = bigquery.Client()
+
+
+
+"""
+ // Open time
+ // Open
+ // High
+ // Low
+ // Close
+ // Volume
+ // Close time
+ // Quote asset volume
+ // Number of trades
+ // Taker buy base asset volume
+ // Taker buy quote asset volume
+ // Ignore.
+  """
