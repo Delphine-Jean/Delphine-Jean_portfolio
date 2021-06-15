420e1d5a560ee3b24d02ff7eaeebba03348e366f
Delphine-Jean <delphinejean92@yahoo.fr>
2021-06-15 12:56:55 +0200

    Day 14 - Binance crypto etl - check function datasets bigquery

--- Portfolio_Data/Data_engineering/Binance_Project_Bot/main.py
+++ Portfolio_Data/Data_engineering/Binance_Project_Bot/main.py
@@ -74,6 +74,18 @@ else:
 
     client = bigquery.Client()
 
+    datasets = list(client.list_datasets())
+    project = client.project
+
+    if datasets:
+        print('Dataset in project {}'.format(project))
+        for dataset in datasets:
+            print('\t{}'.format(dataset.dataset_id))
+    else:
+        print('{} project does not contain any datasets'.format(project))
+
+
+
 
 
 
