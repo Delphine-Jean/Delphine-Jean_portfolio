c7da0ed9221a9f541702c93e27fc14208dc8f2ea
Delphine-Jean <delphinejean92@yahoo.fr>
2021-06-22 22:58:00 +0200

    ETL-DW-Cryptos - export function json

--- Portfolio_Data/Data_engineering/ETL-DW-cryptos/airflow-docker/dags/main.py
+++ Portfolio_Data/Data_engineering/ETL-DW-cryptos/airflow-docker/dags/main.py
@@ -1,23 +1,27 @@
 from  binance_client import *
-
+import pandas as pd
+import config
 
 def default_args():
     pass
 
 
 def get_klines():
-
     binances = BinanceAPI(config.API_KEY, config.API_SECRET)
 
     response = binances.get_klines("BTCUSDT", "1m", startTime="06/01/2021 00:00:00", endTime="06/12/2021 00:00:00")
     data = pd.DataFrame(response)
 
     data.columns = ['open_time',
-              'open', 'high', 'low', 'clsoe', 'volume',
+              'open', 'high', 'low', 'close', 'volume',
               'close_time', 'quote asset volume', 'num_trades',
               'taker_base_vol', 'taker_quote_vol', 'ignore']
 
-    data.to_json('./export.json', orient='index')
+
+    # storing the data in JSON format
+    data.to_json(r"C:\Users\delph\Documents\git\Jedha_bootcamp\Portfolio_Data\Data_engineering\ETL-DW-cryptos\airflow-docker\dags\files\export_klines.json", orient='index')
+
+    # displaying the DataFrame
     print(data)
 
-    get_klines()
+get_klines()
