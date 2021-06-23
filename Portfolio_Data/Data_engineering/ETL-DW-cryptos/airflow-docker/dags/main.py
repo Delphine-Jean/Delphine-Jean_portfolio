02d9bd71df39009f6bf1950a095d8a02a6e6e895
Delphine-Jean <delphinejean92@yahoo.fr>
2021-06-23 23:57:45 +0200

    ETL-DW-Cryptos add export csv klines files

--- Portfolio_Data/Data_engineering/ETL-DW-cryptos/airflow-docker/dags/main.py
+++ Portfolio_Data/Data_engineering/ETL-DW-cryptos/airflow-docker/dags/main.py
@@ -1,6 +1,10 @@
 from  binance_client import *
 import pandas as pd
 import config
+import datetime
+import os
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 
 def default_args():
     pass
@@ -8,6 +12,7 @@ def default_args():
 
 def get_klines():
     binances = BinanceAPI(config.API_KEY, config.API_SECRET)
+
 
     response = binances.get_klines("BTCUSDT", "1m", startTime="06/01/2021 00:00:00", endTime="06/12/2021 00:00:00")
     data = pd.DataFrame(response)
@@ -17,11 +22,18 @@ def get_klines():
               'close_time', 'quote asset volume', 'num_trades',
               'taker_base_vol', 'taker_quote_vol', 'ignore']
 
-
-    # storing the data in JSON format
-    data.to_json(r"C:\Users\delph\Documents\git\Jedha_bootcamp\Portfolio_Data\Data_engineering\ETL-DW-cryptos\airflow-docker\dags\files\export_klines.json", orient='index')
+    data.open_time = pd.to_datetime(data.open_time, unit='ms')
+    data.close_time = pd.to_datetime(data.close_time, unit='ms')
+    data.set_index('open_time', inplace=True)
+    # storing the data in CSV format
+    current_date = datetime.datetime.now()
+    filename = str(current_date.day) + str(current_date.month) + str(current_date.year)
+    current_path = os.getcwd()
+    data.to_csv(str(current_path) + r"\files\export_klines" + str(filename + "_.csv"))
 
     # displaying the DataFrame
     print(data)
+
+
 
 get_klines()
